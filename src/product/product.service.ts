7c32116274dea769c89448200a102f33a4c87df8
priyankraychura <priyankraychura@gmail.com>
2026-03-22 11:28:41 +0530

    Add Product module with controller and service implementation

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    private products = [
+        { id: 1, name: "Mobile", price: 20000},
+        { id: 2, name: "Laptop", price: 60000},
+        { id: 3, name: "Tablet", price: 40000}
+    ];
+
+    getAllProducts() {
+        return this.products;
+    }
+
+    getProductById(id: number) {
+        return this.products.find((product) => product.id == id);
+    }
+}
